2a1b7aee98b8a8e5ea08f67d5fa8ed58f97a215c
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-05-21 19:42:27 +0200

    Add tooltip to the grid elements

--- src/components/global/EntryGridComponent.vue
+++ src/components/global/EntryGridComponent.vue
@@ -8,11 +8,13 @@
             v-for="pair in pairElements"
             v-bind:key="pair.key">
 
-            <a v-bind:href="'/anime/' + pair.value.id">
-                <img class="image" v-bind:src="'/api/files/' + pair.value.systemFile.name">
-            </a>
+            <div class="tooltip" v-bind:data-id="pair.value.id">
+                <a v-bind:href="'/anime/' + pair.value.id">
+                    <img class="image" v-bind:src="'/api/files/' + pair.value.systemFile.name">
+                </a>
 
-            <a class="link" v-bind:href="'/anime/' + pair.value.id">{{ pair.key }}</a>
+                <a class="link" v-bind:href="'/anime/' + pair.value.id">{{ pair.key }}</a>
+            </div>
         </div>
     </div>
 </template>
@@ -24,6 +26,9 @@ import KeyValuePair from "@/common/models/KeyValuePair";
 import EntryUtils from "@/common/utilities/EntryUtils";
 import Constants from "@/common/Constants";
 import ImageComponent from "@/components/global/ImageComponent.vue";
+import Tippy, { Instance } from "tippy.js";
+import TranslationUtils from "@/common/utilities/TranslationUtils";
+import _ from "lodash";
 
 @Component({
     components: {
@@ -39,12 +44,16 @@ export default class EntryGridComponent extends Vue {
 
     mounted() {
         this.elementClasses = `column is-${this.colspan}`;
-
         this.setPairElements();
+    }
+
+    updated() {
+        this.initTooltip();
     }
 
     @Watch("entries") onElementChange() {
         this.setPairElements();
+        this.initTooltip();
     }
 
     private setPairElements() {
@@ -54,6 +63,36 @@ export default class EntryGridComponent extends Vue {
                 value: x
                 })
             );
+        });
+    }
+
+    private initTooltip() {
+        this.$nextTick(() => {
+            const entries = this.entries;
+
+            Tippy(document.querySelectorAll("div.entry-grid div div.tooltip"), {
+                placement: "right-start",
+                allowHTML: true,
+                theme: "entryGrid",
+                onCreate(instance: Instance) {
+                    const id = instance.reference.getAttribute("data-id");
+                    const data = entries.find(x => x.id.toString() === id) as BaseEntry;
+
+                    const tags = _.take(data.tags, 3);
+                    let tagElements: string = "";
+
+                    tags.forEach(x => {
+                        tagElements += `<span class="tag is-rounded">${_.escape(TranslationUtils.translate(x.translationKey))}</span>`;
+                    });
+
+                    let template = `
+                        <p>${_.escape(TranslationUtils.translate(data.type))}</p>
+                        <p>${_.escape(TranslationUtils.translate(data.status))}</p>
+                        <div class="tags">${tagElements}</div>`;
+
+                    instance.setContent(template);
+                }
+            });
         });
     }
 }
