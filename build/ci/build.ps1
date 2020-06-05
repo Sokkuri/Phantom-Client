cc560406f449df235d29fa0c9af689b061f4f3cd
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-06-05 11:48:39 +0200

    Update build.ps1

--- build/ci/build.ps1
+++ build/ci/build.ps1
@@ -1,14 +1,22 @@
 $projectLocation = "../../";
 
-function prepareProject {
-    npm install
+function runBuild {
+    execute("npm install");
+    execute("npm run lint");
+    execute("npm run build");
 }
 
-function runBuild() {
-    npm run lint
-    npm run build
+function execute {
+    param (
+        [string]$command
+    )
+
+    Invoke-Expression $command
+
+    if ($LASTEXITCODE -ne 0) {
+        exit 1;
+    }
 }
 
 Set-Location $projectLocation;
-prepareProject
 runBuild
