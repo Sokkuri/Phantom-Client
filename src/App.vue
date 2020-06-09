6982b9d3b9a79decae95f75d1652932f5a557e32
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-06-09 13:11:35 +0200

    Replace forkawesome with fontawesome

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
 <template>
     <div id="app">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css" integrity="sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=" crossorigin="anonymous">
         <div ref="apiOffline" class="offline" hidden>
             <div class="container">
                 <div class="columns is-multiline">
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import i18n from "./i18n";
 import router from "./router";
+
+library.add(far);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
