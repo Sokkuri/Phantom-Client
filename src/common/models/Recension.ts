d5da07da190aca836da698921f75c0a5d9c32a1f
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-06-21 14:52:58 +0200

    Update models

--- src/common/models/Recension.ts
+++ src/common/models/Recension.ts
@@ -2,11 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import BaseModel from "./BaseModel";
-
-export default class UserList extends BaseModel {
-    overallRating: number;
-    status: string;
-    currentEpisode: number;
-    addingDate: Date;
+export default class Recension {
+    content: string;
 }
