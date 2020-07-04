62f8e4866d1566111770268edd9395095cf96b70
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-07-04 17:27:55 +0200

    Add font awesome icon whitelist

---
+++ src/FontAwesomeIcons.ts
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPlayCircle, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faCheckCircle, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import Vue from "vue";
+
+export default class FontAwesomeIcons {
+    public static init() {
+        library.add(
+            faPlayCircle,
+            faTrashAlt,
+            faCheckCircle,
+            faPlusCircle);
+
+        Vue.component("font-awesome-icon", FontAwesomeIcon);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import FontAwesomeIcons from "@/FontAwesomeIcons";
 import Vue from "vue";
 import VueI18n from "vue-i18n";
 import VueRouter from "vue-router";
@@ -12,9 +10,7 @@ import App from "./App.vue";
 import i18n from "./i18n";
 import router from "./router";
 
-library.add(far);
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+FontAwesomeIcons.init();
 
 Vue.config.productionTip = false
 
