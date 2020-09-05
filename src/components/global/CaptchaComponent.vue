1e137eec4d9c41a8a1d47c21b3324e783c77feea
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-09-05 17:32:29 +0200

    Implement invisible captcha mode

--- src/components/global/CaptchaComponent.vue
+++ src/components/global/CaptchaComponent.vue
@@ -3,12 +3,23 @@
    - file, You can obtain one at https://mozilla.org/MPL/2.0/. -->
 
 <template>
-    <vue-hcaptcha @verify="onVerify" :sitekey="sitekey" :theme="'dark'"></vue-hcaptcha>
+    <div class="captcha-component">
+        <vue-hcaptcha ref="captcha" @verify="onVerify" :sitekey="sitekey" :theme="'dark'" :size="size"></vue-hcaptcha>
+        <div class="notification" v-if="!visible">
+            <p class="has-text-centered">{{ $t("components.captcha.notification.title") }}</p>
+            <div class="level">
+                <div class="level-item">
+                    <a href="https://hcaptcha.com/privacy" target="_blank" rel="noopener noreferrer">{{ $t("components.captcha.notification.privacyPolicy") }}</a>
+                    <a href="https://hcaptcha.com/terms" target="_blank" rel="noopener noreferrer">{{ $t("components.captcha.notification.termsOfService") }}</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import VueHcaptcha from "@hcaptcha/vue-hcaptcha";
+import VueHcaptcha, { HCaptchaMethods } from "@hcaptcha/vue-hcaptcha";
 import Settings from "@/Settings";
 
 @Component({
@@ -17,9 +28,23 @@ import Settings from "@/Settings";
     },
 })
 export default class CaptchaComponent extends Vue {
+    @Prop({default: true}) private visible: boolean;
+
     private sitekey = Settings.HCaptcha.SiteKey;
+    private size = "normal";
+
+    created() {
+        if (!this.visible) {
+            this.size = "invisible";
+        }
+    }
+
+    public execute() {
+        (this.$refs.captcha as unknown as HCaptchaMethods).execute();
+    }
 
     private onVerify(response: string) {
+        this.$emit("verify", response);
         this.$emit("input", response);
     }
 }
