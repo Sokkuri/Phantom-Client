28060a05be4316867a063b2a2fc5f3d5847cce4c
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-09-05 17:33:25 +0200

    Add auto input focus

--- src/components/global/InputComponent.vue
+++ src/components/global/InputComponent.vue
@@ -3,11 +3,15 @@
    - file, You can obtain one at https://mozilla.org/MPL/2.0/. -->
 
 <template>
-    <ValidationProvider
-        v-bind:rules="rules"
-        v-slot="{ errors }">
-        <input @input="onInput()" v-model="value" class="input is-error" v-bind:class="{ errored: errors.length > 0 }" v-bind:type="type" v-bind:maxlength="maxlength" />
-    </ValidationProvider>
+    <div class="field">
+        <ValidationProvider
+            v-bind:rules="rules"
+            v-slot="{ errors }"
+            v-bind:vid="name">
+            <label class="label" v-if="label">{{ label }}</label>
+            <input ref="inputElement" @input="onInput()" v-model="value" class="input is-error" v-bind:class="{ errored: errors.length > 0 }" v-bind:type="type" v-bind:name="name" v-bind:maxlength="maxlength" />
+        </ValidationProvider>
+    </div>
 </template>
 
 <script lang="ts">
@@ -15,11 +19,21 @@ import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 
 @Component
 export default class InputComponent extends Vue {
+    @Prop({required: true}) private name: string;
+    @Prop({default: ""}) private label: string;
     @Prop({default: "text"}) private type: string;
     @Prop({default: 255}) private maxlength: number;
+    @Prop({default: false}) private focus: boolean;
     @Prop({default: ""}) private rules: string;
 
     private value: unknown = null;
+
+    mounted() {
+        if (this.focus) {
+            const element = this.$refs.inputElement as HTMLInputElement;
+            element.focus();
+        }
+    }
 
     private onInput() {
         this.$emit("input", this.value);
