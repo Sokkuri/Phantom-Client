4fb50eff6dd1196cfecda692ac11437a8e92d0c4
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-11-14 13:13:13 +0100

    Reworked translations structure

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,26 +2,23 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import LocalTranslations from "@/translations/Translations";
+import { Translations } from "@sokkuri/common";
 import Vue from "vue";
 import VueI18n, { LocaleMessages } from "vue-i18n";
 
 Vue.use(VueI18n);
 
-function loadLocaleMessages (): LocaleMessages {
-  const locales = require.context("./locales", true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  const messages: LocaleMessages = {};
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
-    }
-  });
-  return messages;
+function getTranslations() {
+    const translations: LocaleMessages = {
+        "de": Object.assign({}, Translations.DE, LocalTranslations.DE)
+    };
+
+    return translations;
 }
 
 export default new VueI18n({
-  locale: "de",
-  fallbackLocale: "de",
-  messages: loadLocaleMessages()
+    locale: "de",
+    fallbackLocale: "de",
+    messages: getTranslations()
 });
